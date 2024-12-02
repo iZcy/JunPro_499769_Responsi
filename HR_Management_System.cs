3461c800caab8246dc6e4f58a779515be03de0b9
Yitzhak Edmund Tio Manalu <121650790+iZcy@users.noreply.github.com>
2024-12-02 08:02:35 +0700

    Add project files.

---
+++ HR_Management_System.cs
@@ -0,0 +1,20 @@
+namespace JunPro_499769_Responsi
+{
+    public partial class HR_Management_System : Form
+    {
+        public HR_Management_System()
+        {
+            InitializeComponent();
+        }
+
+        private void HR_Management_System_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hello World!");
+        }
+    }
+}
