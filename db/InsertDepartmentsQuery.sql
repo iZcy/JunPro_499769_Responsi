0187cf20c38aa024d4e85a81246b6e78f92cf4cd
Yitzhak Edmund Tio Manalu <121650790+iZcy@users.noreply.github.com>
2024-12-02 09:40:39 +0700

    feat: missing db

---
+++ db/InsertDepartmentsQuery.sql
@@ -0,0 +1,5 @@
+INSERT INTO public.departemen (nama_departemen) VALUES ('Human Resources');
+INSERT INTO public.departemen (nama_departemen) VALUES ('Engineer');
+INSERT INTO public.departemen (nama_departemen) VALUES ('Developer');
+INSERT INTO public.departemen (nama_departemen) VALUES ('Product Manager');
+INSERT INTO public.departemen (nama_departemen) VALUES ('Finance');
---
+++ db/InsertJabatanQuery.sql
@@ -0,0 +1,4 @@
+INSERT INTO public.jabatan (nama_jabatan, salary) VALUES ('Supervisor', 100000);
+INSERT INTO public.jabatan (nama_jabatan, salary) VALUES ('Manager', 75000);
+INSERT INTO public.jabatan (nama_jabatan, salary) VALUES ('Senior', 50000);
+INSERT INTO public.jabatan (nama_jabatan, salary) VALUES ('Junior', 25000);
